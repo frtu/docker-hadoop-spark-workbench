9753a0a4dfbd64a673fd9a826328662ccb81f846
Jens Gotthardsen <jens.gotthardsen@gmail.com>
2018-11-19 23:32:12 +0100

    Self ref :)

--- install-docker.sh
+++ install-docker.sh
@@ -10,4 +10,3 @@ apt-get install -y docker-ce
 apt-get install -y build-essential
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/gotthardsen/docker-hadoop-spark-workbench.git
