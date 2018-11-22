1293e0cd21a04dff40238658a25f3834d71cc0fd
Jens Gotthardsen <jens.gotthardsen@gmail.com>
2018-11-22 17:29:46 +0100

    Rettet således IP rent faktisk findes

--- start-hadoop-spark-workbench.sh
+++ start-hadoop-spark-workbench.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d
 
-my_ip=`ip route get 1|awk '{print $NF;exit}'`
+my_ip=`ip route get 255.255.255.255 | grep -Po '(?<=src )(\d{1,3}.){4}'`
 echo "Namenode: http://${my_ip}:50070"
 echo "Datanode: http://${my_ip}:50075"
 echo "Spark-master: http://${my_ip}:8080"
